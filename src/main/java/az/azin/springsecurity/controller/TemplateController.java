64b2ebea56c828334b69af36bc3dfac8d616ae09
Tamerlan <tamerlan.mustafayev@rabita.az>
2022-12-05 16:42:05 +0400

    test commit

--- src/main/java/az/azin/springsecurity/controller/TemplateController.java
+++ src/main/java/az/azin/springsecurity/controller/TemplateController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TemplateController {
 
     @GetMapping("login")
-    public String gerLoginView() {
+    public String getLoginView() {
         return "login";
     }
 
